379774353a3c9d24c2867594903d3878321aaafb
HINAKAGURA <84380354+HINAKAGURA@users.noreply.github.com>
2021-05-20 11:32:08 +0900

    Add files via upload

--- search.py
+++ search.py
@@ -5,6 +5,13 @@
 # 検索ソース
 source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
+# CSVファイル読み込み
+path = "member.csv"
+with open(path,mode="r") as f:
+    for mem in f:
+        source.append(mem.replace("\n",""))
+
+
 ### 検索ツール
 def search():
     word =input("鬼滅の登場人物の名前を入力してください >>> ")
@@ -13,7 +20,15 @@ def search():
 
         print("{}が見つかりました".format(word))
     else:
-        print("{}はリストにありません".format(word))
+        print("{}はリストにありません。追加します".format(word))
+        source.append(word)
+        
+    #登場人物リスト（更新版）を出力
+    path_new="new_member.csv"
+    with open(path_new,mode="w") as f:
+        for mem in source:
+            f.write(mem + "\n")
+
 
 if __name__ == "__main__":
     search()
